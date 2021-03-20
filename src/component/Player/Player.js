7cce7518f9b177e2f90cafbd4578df53fc9d196f
Asif Mahmud <asifmahmud3472@gmail.com>
2021-03-20 15:28:22 +0600

    modified

--- src/component/Player/Player.js
+++ src/component/Player/Player.js
@@ -7,7 +7,7 @@ const Player = (props) => {
     let {full_name, img, birth, country} = props.player;
     return (
         <div className="card col-md-3 m-4 p-0" style={{width: "18rem"}}>
-            <img className="card-img-top" src={img} alt="picture"/>
+            <img className="card-img-top" src={img} alt=""/>
             <div className="card-body">
                 <h5 className="card-title">{full_name}</h5>
                 <p className="card-text"><FontAwesomeIcon icon={faBirthdayCake} /> {birth}</p>
